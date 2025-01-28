2a4d94a10f7293ed26ebcd14c94003c8fedc1707
Yono <yono@ideku.id>
2025-01-28 12:33:26 +0700

    init first service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3001 },
+    },
+  );
+  await app.listen();
+  console.log(`Auth service is running on port ${process.env.PORT || 3001}`);
 }
 bootstrap();
